4d6b7382d07940aa7a870bcfd25cbe368dbaaa0f
Padma Patil <padma@email.com>
2018-10-26 10:27:38 -0700

    add p to html, try and fail for js array annalysis.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,13 @@ function numberToConvert(number){
   var range = [];
   for (var i = 0; i <= number; i++){
     range.push(i);
+    // if (number % 3 === 0){
+    //   range.push("Sorry");
+    // } else if (number === 1 || 10 || 11 || 21 || 31 || 41){
+    //   range.push("Boop");
+    // } else if (number === 0 || 20 || 30 || 40 || 50){
+    //   range.push("Beep");
+    // }
   }
   return range;
 }
